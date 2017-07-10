f7597f52396e79a7cd45d319583f5f29b5dabd2b
Adrien M <hadolf@gmail.com>
2017-07-10 21:36:57 +0000

    add delegator code + limiting minimum weight in config

--- promoted.js
+++ promoted.js
@@ -10,6 +10,38 @@ var lastUpvoteTime = new Date(save[1].timestamp)
 console.log(lastUpvoteTime)
 console.log(save[0]+' is the last transaction # processed')
 
+if (config.delegators) {
+  // calculating and publishing shares of the @promoted account
+  // in order to share curation rewards
+  var delegatorsShares = []
+  for (var i = 0; i < config.delegators.length; i++) {
+    steem.api.getVestingDelegations(config.delegators[i], 999, 999, function(err, delegations) {
+      for (var i = 0; i < delegations.length; i++) {
+        if (delegations[i].delegatee == 'promoted') {
+          delegatorsShares[delegations[i].delegator] = {vesting_shares: delegations[i].vesting_shares, min_delegation_time: delegations[i].min_delegation_time}
+        }
+      }
+    });
+  }
+
+  setTimeout(function(){
+    var totalVests = 0;
+    for (var delegator in delegatorsShares) {
+      totalVests += parseInt(delegatorsShares[delegator].vesting_shares.split(' ')[0])
+    }
+    for (var delegator in delegatorsShares) {
+      delegatorsShares[delegator].percent = parseInt(delegatorsShares[delegator].vesting_shares.split(' ')[0]) / totalVests
+    }
+    var markdown = '|Delegator|Time|VESTS|Share|\n'
+    markdown += '|---|---|---|---|\n'
+    delegatorsShares = delegatorsShares.sort(function(a, b){console.log(a,b);return a.percent-b.percent});
+    for (var delegator in delegatorsShares) {
+      markdown += '|'+delegator+'|'+delegatorsShares[delegator].min_delegation_time+'|'+delegatorsShares[delegator].vesting_shares+'|'+delegatorsShares[delegator].percent+'|\n'
+    }
+    console.log(markdown)
+  },10000)
+}
+
 setInterval(function() {
   steem.api.getAccountHistory('null', save[0]+config.limit, config.limit, function(err, history) {
     if (err) throw err;
@@ -29,7 +61,6 @@ function upvote(transfer) {
     setTimeout(function() {upvote(transfer)}, config.retryTime)
     return;
   }
-  lastUpvoteTime = new Date()
   var author = transfer[1].op[1].memo.split('/')[0].replace('@','')
   var permlink = transfer[1].op[1].memo.split('/')[1]
   var amount = transfer[1].op[1].amount.split(' ')[0]
@@ -39,12 +70,14 @@ function upvote(transfer) {
   if (amount < config.maxPrice) weight *= (amount/config.maxPrice) // reducing linearly
   weight = Math.ceil(weight)
   if (weight > config.maxWeight) weight = config.maxWeight
-
+  if (weight < config.minWeight) weight = 0;
   if (!permlink || !author || !weight) {
-    console.log(transfer[0], transfer[1].timestamp, transfer[1].block, transfer[1].op)
+    console.log('Invalid', transfer[0], transfer[1].timestamp, transfer[1].block, transfer[1].op)
+    save = transfer
     return
   }
   console.log('Voting '+weight/100+'% '+author+'/'+permlink.substr(0,10))
+  lastUpvoteTime = new Date()
   steem.broadcast.vote(
     config.privkey,
     config.user,
